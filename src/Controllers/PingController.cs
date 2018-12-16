aa112031463eeaa77e771ed1ce3c2b144044f605
pacjman <pacjman@cerberix.net>
2018-12-16 09:32:43 -0800

    add asp.net core 2.1 web api template

---
+++ src/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "PONG";
+        }
+    }
+}
