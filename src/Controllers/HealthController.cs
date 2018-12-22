963fa189415cc175d7539731bd1e71db77c4aae5
pacjman <pacjman@cerberix.net>
2018-12-22 08:12:26 -0800

    add cerberix health check

---
+++ src/Controllers/HealthController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Cerberix.HealthCheck.Core;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthController : Controller
+	{
+        private readonly HealthCheckService _healthCheckService;
+
+		public HealthController(
+            HealthCheckService healthCheckService
+			)
+		{
+            _healthCheckService = healthCheckService;
+        }
+
+        [HttpGet]
+        public async Task<HealthCheckResult[]> HealthDetails()
+        {
+            var results = await _healthCheckService.GetResults();
+            return results;
+        }
+    }
+}
--- src/Startup.cs
+++ src/Startup.cs
@@ -19,6 +19,46 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddTransient((container) =>
+            {
+                return new Cerberix.HealthCheck.Core.HealthCheckService(
+                    healthChecks: new Cerberix.HealthCheck.Core.IHealthCheck []
+                    {
+                        //
+                        //  TODO: inject your health checks here
+                        //
+
+                        ////  PM> Install-Package Cerberix.HealthCheck.HttpEndpoint -Version 1.0.0
+                        //new Cerberix.HealthCheck.HttpEndpoint.HttpGetEndpointHealthCheck(
+                        //    description: "http endpoint health check (postman-echo)",
+                        //    endpoint: "https://postman-echo.com/delay/2",
+                        //    connectTimeout: 5
+                        //    ),
+
+                        ////  PM> Install-Package Cerberix.HealthCheck.RedisConnection -Version 1.0.0
+                        //new Cerberix.HealthCheck.RedisConnection.RedisConnectionHealthCheck(
+                        //    description: "redis connection health check (localhost:6379)",
+                        //    connectionString: "localhost:6379",
+                        //    connectTimeout: 5,
+                        //    connectRetry: 3
+                        //    ),
+
+                        ////  PM> Install-Package Cerberix.HealthCheck.SqlConnection -Version 1.0.0
+                        //new Cerberix.HealthCheck.SqlConnection.SqlConnectionHealthCheck(
+                        //    description: "dockerized sql server connection (localhost:1433)",
+                        //    connectionString: "Data Source=tcp:localhost,1433;Integrated Security=False;User Id=sa;Password=pw;",
+                        //    connectTimeout: 10
+                        //    ),
+
+                        //  PM> Install-Package Cerberix.HealthCheck.MySqlConnection -Version 1.0.0
+                        //new Cerberix.HealthCheck.MySqlConnection.MySqlConnectionHealthCheck(
+                        //    description: "dockerized mysql server connection (localhost:3306)",
+                        //    connectionString: "server=127.0.0.1;port=3306;uid=root;pwd=my-secret-pw;",
+                        //    connectTimeout: 10
+                        //    ),
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
